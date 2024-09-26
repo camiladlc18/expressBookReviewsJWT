3cacb69b5abfc6ae456b1ffa4b4479d68d533297
camiladlc18 <camiladlc95@gmail.com>
2024-09-26 19:38:44 -0400

    Adding code to the Book App

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios');
 
 public_users.post("/register", (req, res) => {
     const username = req.body.username;
@@ -65,6 +65,7 @@ public_users.get('/title/:title', function (req, res) {
     }
 });
 
+
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
@@ -73,6 +74,52 @@ public_users.get('/review/:isbn', function (req, res) {
         return res.send(books[isbn].reviews);
     } else {
         return res.status(404).json({ message: "Book not found" });
+    }
+});
+
+
+
+//PROMISES ASYNC-AWAIT --> TASK 10 - 13
+
+public_users.get('/', async function (req, res) {
+    try {
+        const response = await axios.get('http://localhost:5000/'); 
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching book list." });
+    }
+});
+
+
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn
+    try {       
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`); 
+        res.send(response.data);
+    } catch (error) {
+        res.status(500).send({ message: "Error fetching book list." });
+    }
+});
+
+
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author;
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        res.send(response.data);
+    } catch (error) {
+        res.status(404).json({ message: "El autor no existe" });
+    }
+});
+
+
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title;
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        res.send(response.data);
+    } catch (error) {
+        res.status(404).json({ message: "El título no existe" });
     }
 });
 
